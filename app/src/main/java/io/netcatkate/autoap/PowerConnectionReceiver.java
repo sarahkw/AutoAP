78301deccdcd6af2cc4987bf8424035ed1b3f81e
sarah <netcatkate@gmail.com>
2020-12-28 15:11:57 -0800

    Check Wifi before acting

--- app/src/main/java/io/netcatkate/autoap/PowerConnectionReceiver.java
+++ app/src/main/java/io/netcatkate/autoap/PowerConnectionReceiver.java
@@ -3,12 +3,26 @@ package io.netcatkate.autoap;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
-            ApControl.start();
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+            Network activeNetwork = connectivityManager.getActiveNetwork();
+            if (activeNetwork == null) {
+                ApControl.start();
+            } else {
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork);
+                if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    // No-op
+                } else {
+                    ApControl.start();
+                }
+            }
         } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
             ApControl.stop();
         }
