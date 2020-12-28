c526d546aaf7b0eae5d0b467443266142fb0e9f2
sarah <netcatkate@gmail.com>
2020-12-28 14:57:58 -0800

    It works

--- app/src/main/java/io/netcatkate/autoap/ApControl.java
+++ app/src/main/java/io/netcatkate/autoap/ApControl.java
@@ -2,12 +2,24 @@ package io.netcatkate.autoap;
 
 import android.util.Log;
 
+import java.io.IOException;
+
 public class ApControl {
     public static void start() {
         Log.i("ApControl", "start");
+        try {
+            Runtime.getRuntime().exec(new String[] { "su", "-c", "service", "call", "connectivity", "24", "i32", "0", "i32", "0", "i32", "0", "s16", "dummy" });
+        } catch (IOException e) {
+            Log.e("ApControl", "fail");
+        }
     }
 
     public static void stop() {
         Log.i("ApControl", "stop");
+        try {
+            Runtime.getRuntime().exec(new String[] { "su", "-c", "service", "call", "connectivity", "25", "i32", "0", "s16", "dummy" });
+        } catch (IOException e) {
+            Log.e("ApControl", "fail");
+        }
     }
 }
